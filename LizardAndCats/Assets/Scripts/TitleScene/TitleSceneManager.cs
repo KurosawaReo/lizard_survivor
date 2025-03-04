4ff4e3a862b97e4ff50d4dd2d9cd40972072bf05
marumari025 <etouyuma@gmail.com>
2025-03-04 15:38:46 +0900

    スクリプトにボタンの列挙対とスタートボタン追加

--- LizardAndCats/Assets/Scripts/TitleScene/TitleSceneManager.cs
+++ LizardAndCats/Assets/Scripts/TitleScene/TitleSceneManager.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Const;
+
+enum E_BUTTON
+{
+    START,
+    FIRST,
+    SECOND,
+    THIRD,
+    ENDLESS,
+
+    COUNT,
+};
+
 
 public class TitleSceneManager : MonoBehaviour
 {
+    [SerializeField] GameObject[] ButtonObj = new GameObject[(int)E_BUTTON.COUNT];
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +27,11 @@ public class TitleSceneManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public void TitleButtonOn()
     {
         
     }
